b9e71b6d89298b309e23ce55c3b02bad73f51205
Ayushverma93 <ayush.04850@gmail.com>
2026-05-25 12:05:27 +0530

    fixed install_requires issue

--- setup.py
+++ setup.py
@@ -5,14 +5,5 @@ setup(
     version="0.0.1",
     author="Ayush",
     packages=find_packages(),
-    install_requires=[
-        "pandas",
-        "numpy",
-        "scikit-learn",
-        "flask",
-        "catboost",
-        "xgboost",
-        "dill",
-        "gunicorn"
-    ]
+    install_requires=[]
 )
